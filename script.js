510e195a53a1ba1c697b00249488d28f0132b597
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-24 00:31:04 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
+const lista = document.querySelector('#lista-tarefas');
+const texto = document.querySelector('#texto-tarefa');
+
 function criarLista() {
-  const lista = document.querySelector('#lista-tarefas');
   const adicionarItem = document.createElement('li');
-  lista.innerText = input.value;
-  adicionarItem.classname = 'item';
+  adicionarItem.innerText = texto.value;
+  adicionarItem.className = 'itens';
   lista.appendChild(adicionarItem);
+  texto.value = '';
 }
 const botao = document.querySelector('#criar-tarefa');
 botao.addEventListener('click', criarLista);
+ 
+
+/* //Requisito 5 e 6
+let btnAdicionar = document.querySelector("#criar-tarefa");
+let btnSalvar = document.querySelector("#salvar-tarefas");
+let btnApagarTudo = document.querySelector("#apaga-tudo");
+let btnRemoverFinalizados = document.querySelector("#remover-finalizados");
+let btnRemoverSelecionadas = document.querySelector("#remover-selecionado");
+let input = document.querySelector('#texto-tarefa'); 
+let ol = document.querySelector("#lista-tarefas");
+let btnSobe = document.querySelector("#mover-cima");
+let btnDece = document.querySelector("#mover-baixo");
+
+function criarTarefa() {
+  let newLi = document.createElement('li');                            
+  ol.appendChild(newLi);
+  newLi.innerText = input.value;
+  newLi.className = "itens"
+  input.value = '';  
+}
+
+btnAdicionar.addEventListener("click", criarTarefa);
+
+//Requisito 7
+function setGray(event) {
+  clearClassesName()
+  if(event.target.classList.contains("selected")){
+    event.target.classList.remove("selected") 
+  }
+  else{
+    event.target.classList.add('selected') 
+  }
+}
+
+let lis = document.querySelector("#lista-tarefas");
+lis.addEventListener("click", setGray); */
