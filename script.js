09a3076d0b3748147ae6ef4e13cfd85541cfe4b8
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-24 00:48:00 -0300

    requisito 7

--- script.js
+++ script.js
@@ -10,39 +10,12 @@ function criarLista() {
 }
 const botao = document.querySelector('#criar-tarefa');
 botao.addEventListener('click', criarLista);
- 
 
-/* //Requisito 5 e 6
-let btnAdicionar = document.querySelector("#criar-tarefa");
-let btnSalvar = document.querySelector("#salvar-tarefas");
-let btnApagarTudo = document.querySelector("#apaga-tudo");
-let btnRemoverFinalizados = document.querySelector("#remover-finalizados");
-let btnRemoverSelecionadas = document.querySelector("#remover-selecionado");
-let input = document.querySelector('#texto-tarefa'); 
-let ol = document.querySelector("#lista-tarefas");
-let btnSobe = document.querySelector("#mover-cima");
-let btnDece = document.querySelector("#mover-baixo");
-
-function criarTarefa() {
-  let newLi = document.createElement('li');                            
-  ol.appendChild(newLi);
-  newLi.innerText = input.value;
-  newLi.className = "itens"
-  input.value = '';  
-}
-
-btnAdicionar.addEventListener("click", criarTarefa);
-
-//Requisito 7
-function setGray(event) {
-  clearClassesName()
-  if(event.target.classList.contains("selected")){
-    event.target.classList.remove("selected") 
+function mudarCor(event) {
+  const itemLista = lista.children;
+  for (let index = 0; index < itemLista; index += 1) {
+    itemLista[index].getElementsByClassName.backgroundColor = 'white';
   }
-  else{
-    event.target.classList.add('selected') 
-  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
-
-let lis = document.querySelector("#lista-tarefas");
-lis.addEventListener("click", setGray); */
+lista.addEventListener('click', mudarCor);
