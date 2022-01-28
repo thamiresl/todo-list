9592622e6ed9632be5ad7ed33dde90b78dc90808
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-27 23:13:08 -0300

    requisito 12

--- script.js
+++ script.js
@@ -13,20 +13,20 @@ botao.addEventListener('click', criarLista);
 function mudarCor(event) {
   let itemLista = lista.children;
   for (let index = 0; index < itemLista.length; index += 1) {
-    document.getElementsByTagName('LI')[index].style.backgroundColor = '#fff';
+    document.getElementsByTagName('li')[index].style.backgroundColor = '#fff';
   }
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 lista.addEventListener('click', mudarCor);
 
  function tarefaCompleta(event) {
-  if (event.target.className === 'completed'){
+  if (event.target.className === 'completed') {
     event.target.classList.remove('completed');
   } else {
   event.target.className = 'completed';
   }
 }
-lista.addEventListener('dblclick', tarefaCompleta); 
+lista.addEventListener('dblclick', tarefaCompleta);
 
 function removerTudo() {
   lista.innerHTML = '';
@@ -42,6 +42,19 @@ function removerConcluidos() {
 }
 const botaoRemoverConcluidos = document.querySelector('#remover-finalizados');
 botaoRemoverConcluidos.addEventListener('click', removerConcluidos);
+
+function salvarLista() {
+  localStorage.setItem('listagem', lista.innerHTML);
+}
+const botaoSalvar = document.querySelector('#salvar-tarefas');
+botaoSalvar.addEventListener('click', salvarLista);
+
+function listaSalva() {
+  lista.innerHTML = localStorage.getItem('listagem');
+}
+listaSalva();
+
 // Referencias
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
 // https://stackoverflow.com/questions/21976227/classlist-add-does-not-work
+// https://www.w3schools.com/jsref/prop_win_localstorage.asp
