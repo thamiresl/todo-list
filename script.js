f0e3e7649c3f71d7ce32ee8c3313972d16ce145b
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-27 21:00:41 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const lista = document.querySelector('#lista-tarefas');
-const texto = document.querySelector('#texto-tarefa');
+let lista = document.querySelector('#lista-tarefas');
+let texto = document.querySelector('#texto-tarefa');
 
 function criarLista() {
   const adicionarItem = document.createElement('li');
   adicionarItem.innerText = texto.value;
-  adicionarItem.className = 'itens';
   lista.appendChild(adicionarItem);
   texto.value = '';
 }
@@ -13,31 +12,36 @@ botao.addEventListener('click', criarLista);
 
 function mudarCor(event) {
   let itemLista = lista.children;
-  for (let index = 0; index < itemLista; index += 1) {
-    itemLista[index].getElementsByClassName.backgroundColor = 'white';
+  for (let index = 0; index < itemLista.length; index += 1) {
+    document.getElementsByTagName('LI')[index].style.backgroundColor = '#fff';
   }
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  event.target.style.backgroundColor = '#808080';
 }
 lista.addEventListener('click', mudarCor);
 
-function tarefaCompleta(event) {
-  if (event.target.style.textDecoration === '') {
-    event.target.classList.add('completed');
-    event.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
-  } else {
-    event.target.style.textDecoration = '';
-    event.target.className = '';
-  }
-} 
-const adicionarItem = document.createElement('li');
-adicionarItem.addEventListener('dblClick', tarefaCompleta);
+//
 
+function tarefaCompleta(event) {
+  event.target.classList.add('completed');
+  event.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+}
+lista.addEventListener('dblClick', tarefaCompleta);
+
+//
 function removerTudo() {
   lista.innerHTML = '';
 }
 const botaoRemoverTudo = document.querySelector('#apaga-tudo');
 botaoRemoverTudo.addEventListener('click', removerTudo);
-
+//
+function removerConcluidos() {
+  const concluidos = document.querySelectorAll('.completed');
+  for (let index = 0; index < concluidos.length; index += 1) {
+    concluidos[index].parentElement.removeChild(concluidos[index]);
+  }
+}
+const botaoRemoverConcluidos = document.querySelector('#remover-finalizados');
+botaoRemoverConcluidos.addEventListener('click', removerTudo);
 // Referencias
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event 
-// https://stackoverflow.com/questions/21976227/classlist-add-does-not-work 
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+// https://stackoverflow.com/questions/21976227/classlist-add-does-not-work
